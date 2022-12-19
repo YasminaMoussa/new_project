1fac795b7b97c8463192ec02dbe28b935e13184e
root <root@localhost.localdomain>
2022-12-19 20:41:32 +0100

    Projet mod

--- filter_csv.py
+++ filter_csv.py
@@ -1,8 +1,9 @@
 # coding: iso-8859-1 -*-
 
 import yfinance as yahooFinance
+liste_actions_moins_risquees = []
+liste_actions_risquees = []
 
-  
 msftInformation      = yahooFinance.Ticker("msft")
 AppleInformation     = yahooFinance.Ticker("aapl")
 GoogleInformation    = yahooFinance.Ticker("goog")
@@ -14,9 +15,18 @@ liste = [msftInformation, AppleInformation, GoogleInformation, FacebookInformati
 maxi = max(msftInformation.info["beta"], AppleInformation.info["beta"], GoogleInformation.info["beta"], FacebookInformation.info["beta"], LVMHFInformation.info["beta"], NFLXInformation.info["beta"])
 
 for elt in liste:
-    print("Le beta de l'action {} est de {}".format(elt.info['shortName'], elt.info['beta']))
+    print("L'action {} appartient au secteur {}. Son prix actuel est de {}$ et son beta est de {}.\n".format(elt.info['shortName'], elt.info['sector'], elt.info['regularMarketPrice'], elt.info['beta']))
     if elt.info['beta'] == maxi:
         nom=elt.info['shortName']
+    if elt.info["beta"] > 1:
+        liste_actions_risquees.append(elt.info['shortName'])
+    elif elt.info["beta"] < 1:
+        liste_actions_moins_risquees.append(elt.info['shortName'])
+
+liste_actions_moins_risquees = ', '.join(liste_actions_moins_risquees)
+liste_actions_risquees = ', '.join(liste_actions_risquees)
 
 print("Parmi ces actions, celle dont le beta est le plus élevé est {}" .format(nom))
+print("Les titres les plus risqués sont :", liste_actions_risquees)
+print("Les titres les moins risqués sont :", liste_actions_moins_risquees)
 
